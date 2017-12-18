4b33c3e074795d4b237130fc72f84f8332baf94a
Mounika <Mounika@mounikas-macbook-air.local>
2017-12-18 12:02:19 +0530

    auto layout for ui

--- Rainfall/CardView.swift
+++ Rainfall/CardView.swift
@@ -30,7 +30,7 @@ class CardView: UIView {
     @IBInspectable var cornerRadius: CGFloat = 2
 
     @IBInspectable var shadowOffsetWidth: Int = 0
-    @IBInspectable var shadowOffsetHeight: Int = 3
+    @IBInspectable var shadowOffsetHeight: Int = 1
     @IBInspectable var shadowColor: UIColor? = UIColor.black
     @IBInspectable var shadowOpacity: Float = 0.5
 
